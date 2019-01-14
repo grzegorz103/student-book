23434fd7389ea5fdad6522a54e0e6ed50f31100c
Seko <seko47@wp.pl>
2019-01-14 19:30:43 +0100

    Dodanie klasy dla dziekana oraz repozytorium

---
+++ src/main/java/edu/uph/ii/platformy/models/Dean.java
@@ -0,0 +1,21 @@
+package edu.uph.ii.platformy.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table ( name = "deans" )
+@PrimaryKeyJoinColumn ( name = "id" )
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dean extends Person
+{
+}
---
+++ src/main/java/edu/uph/ii/platformy/repositories/DeanRepository.java
@@ -0,0 +1,8 @@
+package edu.uph.ii.platformy.repositories;
+
+import edu.uph.ii.platformy.models.Dean;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeanRepository extends JpaRepository< Dean, Long >
+{
+}
